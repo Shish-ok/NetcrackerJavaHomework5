dedc8a5250f991a2917c44cedaf75c5da50ef586
Shish-ok <alexeyshish92@gmail.com>
2022-06-05 19:44:55 +0300

    fixJsFinal

--- src/main/resources/static/js/uploadScript.js
+++ src/main/resources/static/js/uploadScript.js
@@ -2,7 +2,7 @@ function ready () {
     var input = document.querySelector('.input-file');
     var label = input.nextElementSibling
     input.addEventListener('change', function (e) {
-        label.querySelector('span').innerHTML = e.target.value;
+        label.querySelector('span').innerHTML = e.target.value.split('\\').pop();
     });
 }
 
