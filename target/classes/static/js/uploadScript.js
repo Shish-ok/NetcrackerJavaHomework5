34d27a2bcdc72be3d43067ba5ccbee2b010b96b8
Алексей Шишкин <86236873+Shish-ok@users.noreply.github.com>
2022-06-05 21:29:42 +0300

    Delete target directory

--- target/classes/static/js/uploadScript.js
+++
@@ -1,9 +0,0 @@
-function ready () {
-    var input = document.querySelector('.input-file');
-    var label = input.nextElementSibling
-    input.addEventListener('change', function (e) {
-        label.querySelector('span').innerHTML = e.target.value.split('\\').pop();
-    });
-}
-
-document.addEventListener("DOMContentLoaded", ready);
