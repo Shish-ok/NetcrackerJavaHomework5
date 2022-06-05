81d38ac995ab2019b17c025a6de1058c72fb8337
Shish-ok <alexeyshish92@gmail.com>
2022-06-05 19:17:04 +0300

    fixJs

--- src/main/resources/static/js/uploadScript.js
+++ src/main/resources/static/js/uploadScript.js
@@ -8,27 +8,3 @@ function ready () {
 }
 
 document.addEventListener("DOMContentLoaded", ready);
-
-var inputs = document.querySelectorAll('.input-file');
-Array.prototype.forEach.call(inputs, function (input) {
-    var label = input.nextElementSibling;
-    var labelVal = label.innerHTML;
-
-    input.addEventListener('change', function (e) {
-        var fileName = '';
-        console.log(this.files);
-        if (this.files && this.files.length > 1) {
-            fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
-        }
-        else {
-            fileName = this.files[0].name;
-        }
-
-        if (fileName) {
-            label.querySelector('span').innerHTML = fileName;
-        }
-        else {
-            label.innerHTML = labelVal;
-        }
-    });
-});
--- target/classes/static/js/uploadScript.js
+++ target/classes/static/js/uploadScript.js
@@ -1,34 +1,8 @@
 function ready () {
     let input = document.querySelector('.input-file');
-    let lable = input.nextElementSibling;
-
     input.addEventListener('change', function (e) {
-        label.innerHTML = input.innerHTML;
+        console.log(input.innerHTML);
     });
 }
 
 document.addEventListener("DOMContentLoaded", ready);
-
-var inputs = document.querySelectorAll('.input-file');
-Array.prototype.forEach.call(inputs, function (input) {
-    var label = input.nextElementSibling;
-    var labelVal = label.innerHTML;
-
-    input.addEventListener('change', function (e) {
-        var fileName = '';
-        console.log(this.files);
-        if (this.files && this.files.length > 1) {
-            fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
-        }
-        else {
-            fileName = this.files[0].name;
-        }
-
-        if (fileName) {
-            label.querySelector('span').innerHTML = fileName;
-        }
-        else {
-            label.innerHTML = labelVal;
-        }
-    });
-});
